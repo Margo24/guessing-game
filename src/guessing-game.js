8f1e7927bd61eb767091d2af8baa0bfa75316017
‘Margo24’ <margonessa24@gmail.com>
2019-10-14 22:38:08 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+// make a binary search by class
+
+//define variables
+    constructor() {
+        this.min;
+        this.max;
+        this.middle;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.middle = Math.ceil((this.min + this.max)/2);
     }
-
+//reinitialize
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
